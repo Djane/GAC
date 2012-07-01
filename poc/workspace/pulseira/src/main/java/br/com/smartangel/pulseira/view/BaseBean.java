6bedcf95d4a1af7dec40a627da53af71d66eed70
Rogério <Santos>
2012-07-01 11:48:13 -0300

    Criação de página de configuração de usuário

---
+++ poc/workspace/pulseira/src/main/java/br/com/smartangel/pulseira/view/BaseBean.java
@@ -0,0 +1,17 @@
+package br.com.smartangel.pulseira.view;
+
+public class BaseBean {
+
+	private String urlImage = "primefaces-smartangel/images";
+
+	public String getUrlImage() {
+		return urlImage;
+	}
+
+	public void setUrlImage(String urlImage) {
+		this.urlImage = urlImage;
+	}
+	
+	
+	
+}
---
+++ poc/workspace/pulseira/src/main/java/br/com/smartangel/pulseira/view/UsuarioBean.java
@@ -0,0 +1,78 @@
+package br.com.smartangel.pulseira.view;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean 
+@RequestScoped
+public class UsuarioBean extends BaseBean {
+
+	private String username;
+	
+	private String password;
+	
+	private Integer perfil;
+	
+	private List<String> listaLogin = new ArrayList<String>();
+	
+	
+	public UsuarioBean() {
+		super();
+		this.listaLogin.add("XXX");
+	}
+	
+	public void salvar(ActionEvent actionEvent) {          
+		FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Sucesso, Login Salvo", "Login Salvo"));
+    }  
+
+	public String iniciarPagina() {
+		return "cadastrousuario";
+	}
+	
+	
+	public String fechar() {
+		return "menuPrincipal";
+    }  
+	
+	public List<String> getListaLogin() {
+		return listaLogin;
+	}
+
+	public void setListaLogin(List<String> listaLogin) {
+		this.listaLogin = listaLogin;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	public Integer getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(Integer perfil) {
+		this.perfil = perfil;
+	}	
+}
+						
