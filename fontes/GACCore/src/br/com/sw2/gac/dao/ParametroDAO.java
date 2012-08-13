fb1315ef2ed3574482751bee1f0b9d13e9917b88
castilhodaniel <Daniel Castilho@192.168.1.102>
2012-08-13 00:21:57 -0300

    Arquivos correspondentes a tela de parametros

---
+++ fontes/GACCore/src/br/com/sw2/gac/dao/ParametroDAO.java
@@ -0,0 +1,16 @@
+package br.com.sw2.gac.dao;
+
+import br.com.sw2.gac.modelo.Parametro;
+
+/**
+ * @author Daniel Castilho
+ */
+public class ParametroDAO extends BaseDao<Parametro> {
+
+    /**
+     * Construtor Padrao Instancia um novo objeto ParametroDAO.
+     */
+    public ParametroDAO() {
+        super(Parametro.class);
+    }
+}
