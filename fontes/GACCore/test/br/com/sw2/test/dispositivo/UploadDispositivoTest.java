fe6b0250bff0dd5c37eb3e954095df589d3244b3
eduardopax <eduardopax@gmail.com>
2012-08-15 11:47:24 -0300

    Carga de Dispositivos

---
+++ fontes/GACCore/test/br/com/sw2/test/dispositivo/UploadDispositivoTest.java
@@ -0,0 +1,25 @@
+package br.com.sw2.test.dispositivo;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+/**
+ * Testes de carga de dispositivos.
+ *
+ * @author eduardopax
+ *
+ */
+public class UploadDispositivoTest {
+
+	/**
+     * Teste de carga de arquivo com tamanho de colunas inválidas.
+     */
+    @Test
+    public void testCarregarArquivoComColunasFaltantes() {
+
+    	Assert.assertTrue(true);
+    }
+
+}
