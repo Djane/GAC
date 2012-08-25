4a956ecee830bb27e0a34a4c492350b474d817c3
Rogério <Santos>
2012-08-25 00:15:12 -0300

    Preparação para uso do jasper reports

--- fontes/GACWeb/src/br/com/sw2/gac/bean/MenuBean.java
+++ fontes/GACWeb/src/br/com/sw2/gac/bean/MenuBean.java
@@ -1,8 +1,26 @@
 package br.com.sw2.gac.bean;
 
+import java.io.IOException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.jasper.JasperException;
+
+import br.com.sw2.gac.exception.BusinessException;
+import br.com.sw2.gac.jasper.JasperBeanFactory;
+import br.com.sw2.gac.util.LoggerUtils;
 import br.com.sw2.gac.util.MenuItem;
 import br.com.sw2.gac.vo.UsuarioVO;
 
@@ -64,4 +82,30 @@ public class MenuBean extends BaseBean {
 
     }
 
+    /**
+     * Nome: imprimirDispositivosPorEstado
+     * Imprimir dispositivos por estado.
+     *
+     * @param event the event
+     * @see
+     */
+    public void imprimirDispositivosPorEstado(ActionEvent event) {
+
+        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(JasperBeanFactory.createBeanCollection());
+        HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        //httpServletResponse.addHeader("content-disposition", "attachment; filename=dispositivoEstado.pdf");
+        try {
+            JasperPrint jasperPrint = JasperFillManager.fillReport("c:\\temp\\dispositivoEstado.jasper", new HashedMap(), beanCollectionDataSource);
+            ServletOutputStream servletOutputStream = (ServletOutputStream) httpServletResponse.getOutputStream();
+            JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
+        } catch (JRException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+
+    }
 }
