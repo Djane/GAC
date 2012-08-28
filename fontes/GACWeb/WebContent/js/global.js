47e028a477f83b316f9f67b0811104f26dc6e9b3
Rogério <Santos>
2012-08-28 00:28:16 -0300

    Inclusão de jquery de formataçção

--- fontes/GACWeb/WebContent/js/global.js
+++ fontes/GACWeb/WebContent/js/global.js
@@ -1,11 +1,15 @@
+
 $(document).ready(function() {
 	fixarMascara();	
 });
 
 
 function fixarMascara () {
+	
 	$(".cepMask").mask("99999-999");
 	$(".cpfMask").mask("999.999.999/99");
 	$(".telefoneMask").mask("(99)99999-9999");
 	$(".horaMinutoMask").mask("99:99");
+	$(".moedaMask").maskMoney({decimal:",",thousands:"."});
+	
 }
