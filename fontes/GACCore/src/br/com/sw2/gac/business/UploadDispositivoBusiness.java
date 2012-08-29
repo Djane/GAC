b1c09a63b7c6c968c8708bafd4573e5d17710e28
eduardopax <eduardopax@gmail.com>
2012-08-29 10:08:32 -0300

    Carga de Dispositivos

--- fontes/GACCore/src/br/com/sw2/gac/business/UploadDispositivoBusiness.java
+++ fontes/GACCore/src/br/com/sw2/gac/business/UploadDispositivoBusiness.java
@@ -3,12 +3,17 @@ package br.com.sw2.gac.business;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import br.com.sw2.gac.exception.BusinessException;
 import br.com.sw2.gac.exception.BusinessExceptionMessages;
+import br.com.sw2.gac.tools.EstadoDispositivo;
 import br.com.sw2.gac.vo.ArquivoVO;
+import br.com.sw2.gac.vo.DispositivoVO;
+import br.com.sw2.gac.vo.UsuarioVO;
 
 
 /**
@@ -17,8 +22,11 @@ import br.com.sw2.gac.vo.ArquivoVO;
  */
 public class UploadDispositivoBusiness {
 
+	private static final int TOTAL_TIPOS_DISPOSITIVO = 4;
 	private static final int QTDE_COLUNAS_CARGA = 4;
 	private List<String> criticas;
+
+	private SimpleDateFormat  dtParser = new SimpleDateFormat("dd/MM/yyyyy");
 
 	/**
 	 * Processa o arquivo selecionado, realizando a carga dos dispositivos.
@@ -35,13 +43,115 @@ public class UploadDispositivoBusiness {
         	if (!verificaArquivoValido(arquivoDispositivos)) {
         		throw new BusinessException(BusinessExceptionMessages.FALHA_CARGA_DISPOSITIVOS);
         	}
+        	if (!verificaDispositivoDuplicadoCarga(arquivoDispositivos)) {
+        		throw new BusinessException(BusinessExceptionMessages.FALHA_CARGA_DISPOSITIVOS);
+        	}
+        	List<DispositivoVO> dispositivosVO = carregarDispositivosVO(arquivoDispositivos, arquivo.getUsuario());
+        	if (!verificaDispositivoNoBanco(dispositivosVO)) {
+        		throw new BusinessException(BusinessExceptionMessages.FALHA_CARGA_DISPOSITIVOS);
+        	}
+        	inserirDispositivos(dispositivosVO);
 
-        	//monta as entidades para serem persistidas
 		} catch (BusinessException e) {
 			throw e;
 		} catch (Exception e) {
 			throw new BusinessException(BusinessExceptionMessages.FALHA_CARGA_DISPOSITIVOS);
 		}
+    }
+
+    /**
+     * Insere os dispositivos no banco.
+     * @param dispositivos
+     */
+    private void inserirDispositivos(List<DispositivoVO> dispositivos) {
+    	// Cria o dispositivo na base
+    	DispositivoBusiness dispositivoBusiness = new DispositivoBusiness();
+    	for (DispositivoVO dispositivoVO : dispositivos) {
+    		try {
+            	dispositivoBusiness.adicionarNovoDispositivo(dispositivoVO, null);
+            } catch (BusinessException exception) {
+                exception.printStackTrace();
+                throw exception;
+            }
+		}
+    }
+
+    /**
+     * Verifica se existem dispositivos na carga que já existem no banco.
+     * @param carregarDispositivosVO
+     * @return true ou false
+     */
+    private boolean verificaDispositivoNoBanco(
+			List<DispositivoVO> carregarDispositivosVO) {
+    	boolean retorno = true;
+    	DispositivoBusiness dispositivoBusiness = new DispositivoBusiness();
+    	int contLinha = 1;
+
+    	for (DispositivoVO dispositivoVO : carregarDispositivosVO) {
+    		try {
+    			dispositivoBusiness.verificarDispositivoDuplicado(dispositivoVO);
+			} catch (Exception e) {
+				criticas.add("Linha: " + contLinha + " - Erro: dispositivo '" + dispositivoVO.getIdDispositivo() + "' já existente no banco");
+				retorno = false;
+			}
+    		contLinha++;
+		}
+
+    	return retorno;
+	}
+
+	/**
+     * Verifica se existe dispositivo duplicado no arquivo de carga.
+     * @param arquivoDispositivos
+     * @return true ou false
+     */
+    private boolean verificaDispositivoDuplicadoCarga(
+			List<String> arquivoDispositivos) {
+    	List<String> ids = new ArrayList<>();
+    	boolean retorno = true;
+    	String[] carga;
+    	int contLinha = 1;
+    	for (String linha : arquivoDispositivos) {
+			carga = linha.split(";");
+			//se conter o ID entao é duplicado
+			if (ids.contains(carga[0])) {
+				criticas.add("Linha: " + contLinha + " - Erro: dispositivo '" + carga[0] + "' já existente na carga");
+				retorno = false;
+			} else {
+				ids.add(carga[0]);
+			}
+			contLinha++;
+    	}
+
+    	return retorno;
+	}
+
+	/**
+	 * Converte as linhas em objetos VO.
+	 * @param linhas
+	 * @param usuario
+	 * @return lista com dispositivos
+	 * @throws ParseException
+	 */
+    private List<DispositivoVO> carregarDispositivosVO(final List<String> linhas , final UsuarioVO usuario) throws ParseException {
+    	List<DispositivoVO> dispositivos = new ArrayList<>();
+
+    	//monta as entidades para serem persistidas
+    	String[] carga;
+    	for (String linha : linhas) {
+			carga = linha.split(";");
+			DispositivoVO dispositivo = new DispositivoVO();
+			dispositivo.setIdDispositivo(carga[0]);
+
+			dispositivo.setTipoDispositivo(Integer.parseInt(carga[1]));
+			dispositivo.setDataFabricacao(dtParser.parse(carga[2]));
+			dispositivo.setEstadoAtual(EstadoDispositivo.Novo.getValue());
+			dispositivo.setUsuario(usuario);
+
+			dispositivos.add(dispositivo);
+		}
+
+    	return dispositivos;
     }
 
     /**
@@ -63,10 +173,32 @@ public class UploadDispositivoBusiness {
     	int contLinha = 1;
     	for (String linha : arquivoDispositivos) {
 			carga = linha.split(";");
-			if (carga.length < QTDE_COLUNAS_CARGA) {
+			//Verifica a quantidade de colunas
+			if (carga.length != QTDE_COLUNAS_CARGA) {
 				criticas.add("Linha: " + contLinha + " - Erro: Quantidade de colunas inválida");
 				retorno = false;
+			} else {
+				//verifica o tipo de dados das colunas já que o total de colunas é um total valido
+				//Codigo do dispositivo é string entao nao copara.
+				//Tipo do dispositivo
+				try {
+					if (Integer.parseInt(carga[1]) < 0  || Integer.parseInt(carga[1]) > TOTAL_TIPOS_DISPOSITIVO) {
+						throw new Exception();
+					}
+				} catch (Exception e) {
+					criticas.add("Linha: " + contLinha + " - Erro: Valor do Tipo de Dispositivo inválido");
+					retorno = false;
+				}
+
+				//valida a data de fabricacao
+				try {
+					dtParser.parse(carga[2]);
+				} catch (Exception e) {
+					criticas.add("Linha: " + contLinha + " - Erro: Valor da Data de Fabricação inválida");
+					retorno = false;
+				}
 			}
+			contLinha++;
 		}
     	return retorno;
     }
--- fontes/GACCore/test/br/com/sw2/test/dispositivo/UploadDispositivoTest.java
+++ fontes/GACCore/test/br/com/sw2/test/dispositivo/UploadDispositivoTest.java
@@ -9,10 +9,15 @@ import java.util.List;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import br.com.sw2.gac.business.DispositivoBusiness;
 import br.com.sw2.gac.business.UploadDispositivoBusiness;
+import br.com.sw2.gac.exception.BusinessException;
 import br.com.sw2.gac.vo.ArquivoVO;
+import br.com.sw2.gac.vo.PerfilVO;
+import br.com.sw2.gac.vo.UsuarioVO;
 
 
 /**
@@ -22,6 +27,111 @@ import br.com.sw2.gac.vo.ArquivoVO;
  *
  */
 public class UploadDispositivoTest {
+
+	private static final String ID = "abcd0123fg456";
+
+	/**
+	 * Setup a ser executado antes dos testes.
+	 */
+	@Before
+	public void setup() {
+	    // Garantir que o dispositivo que ser� usado nos testes n�o existe na base
+	    apagarDispositivo(ID);
+	}
+
+	/**
+     * Teste de carga de arquivo correto.
+     */
+    @Test
+    public void testCarregarArquivoCorreto() {
+    	String nomeArquivoTemp = "c:/temp/testeOk.txt";
+    	//Cria um arquivo com apenas 2 colunas
+    	List<String> linhasArquivo = new ArrayList<>();
+    	linhasArquivo.add(ID + ";1;01/10/2012;1");
+
+    	try {
+			gerarArquivoTemp(linhasArquivo, nomeArquivoTemp);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			Assert.fail();
+		}
+
+    	ArquivoVO arquivoVO = new ArquivoVO();
+    	arquivoVO.setCaminho(nomeArquivoTemp);
+    	arquivoVO.setUsuario(getUsuario());
+
+    	UploadDispositivoBusiness uploadDispositivoBusiness = new UploadDispositivoBusiness();
+
+    	try {
+    		uploadDispositivoBusiness.processarArquivo(arquivoVO);
+		} catch (Exception exception) {
+			Assert.fail();
+		}
+    }
+
+    /**
+     * Teste de carga de arquivo doms dispositivos duplicados no arquivo de carga.
+     */
+    @Test
+    public void testCarregarArquivoDispositivosDuplicadosNaCarga() {
+    	String nomeArquivoTemp = "c:/temp/testeOk.txt";
+    	//Cria um arquivo com apenas 2 colunas
+    	List<String> linhasArquivo = new ArrayList<>();
+    	linhasArquivo.add(ID + ";1;01/10/2012;1");
+    	linhasArquivo.add(ID + ";1;01/10/2012;1");
+
+    	try {
+			gerarArquivoTemp(linhasArquivo, nomeArquivoTemp);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			Assert.fail();
+		}
+
+    	ArquivoVO arquivoVO = new ArquivoVO();
+    	arquivoVO.setCaminho(nomeArquivoTemp);
+    	arquivoVO.setUsuario(getUsuario());
+
+    	UploadDispositivoBusiness uploadDispositivoBusiness = new UploadDispositivoBusiness();
+
+    	try {
+    		uploadDispositivoBusiness.processarArquivo(arquivoVO);
+		} catch (Exception exception) {
+			List<String> criticas = uploadDispositivoBusiness.recuperarCriticas();
+			Assert.assertEquals("Linha: 2 - Erro: dispositivo 'abcd0123fg456' já existente na carga", criticas.get(0));
+		}
+    }
+
+    /**
+     * Teste de carga de arquivo doms dispositivos no banco.
+     */
+    @Test
+    public void testCarregarArquivoDispositivosNoBanco() {
+    	String nomeArquivoTemp = "c:/temp/testeOk.txt";
+    	//Cria um arquivo com apenas 2 colunas
+    	List<String> linhasArquivo = new ArrayList<>();
+    	linhasArquivo.add(ID + ";1;01/10/2012;1");
+
+    	try {
+			gerarArquivoTemp(linhasArquivo, nomeArquivoTemp);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			Assert.fail();
+		}
+
+    	ArquivoVO arquivoVO = new ArquivoVO();
+    	arquivoVO.setCaminho(nomeArquivoTemp);
+    	arquivoVO.setUsuario(getUsuario());
+
+    	UploadDispositivoBusiness uploadDispositivoBusiness = new UploadDispositivoBusiness();
+
+    	try {
+    		uploadDispositivoBusiness.processarArquivo(arquivoVO);
+		} catch (Exception exception) {
+			List<String> criticas = uploadDispositivoBusiness.recuperarCriticas();
+			Assert.assertEquals("Linha: 1 - Erro: dispositivo 'abcd0123fg456' já existente no banco", criticas.get(0));
+		}
+    }
+
 
 	/**
      * Teste de carga de arquivo com tamanho de colunas inválidas.
@@ -54,6 +164,66 @@ public class UploadDispositivoTest {
     }
 
     /**
+     * Teste de carga de arquivo com a coluna Tipo de Dispositivo errado.
+     */
+    @Test
+    public void testCarregarArquivoComColunaTipoDispositivoErrado() {
+    	String nomeArquivoTemp = "c:/temp/teste2.txt";
+    	//Cria um arquivo com apenas 2 colunas
+    	List<String> linhasArquivo = new ArrayList<>();
+    	linhasArquivo.add("1111;2222;222;222");
+
+    	try {
+			gerarArquivoTemp(linhasArquivo, nomeArquivoTemp);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			Assert.fail();
+		}
+
+    	ArquivoVO arquivoVO = new ArquivoVO();
+    	arquivoVO.setCaminho(nomeArquivoTemp);
+
+    	UploadDispositivoBusiness uploadDispositivoBusiness = new UploadDispositivoBusiness();
+
+    	try {
+    		uploadDispositivoBusiness.processarArquivo(arquivoVO);
+		} catch (Exception exception) {
+			List<String> criticas = uploadDispositivoBusiness.recuperarCriticas();
+			Assert.assertEquals("Linha: 1 - Erro: Valor do Tipo de Dispositivo inválido", criticas.get(0));
+		}
+    }
+
+    /**
+     * Teste de carga de arquivo com a coluna de Data de Fabricacao errada.
+     */
+    @Test
+    public void testCarregarArquivoComColunaDataDeFabricacaoErrada() {
+    	String nomeArquivoTemp = "c:/temp/teste2.txt";
+    	//Cria um arquivo com apenas 2 colunas
+    	List<String> linhasArquivo = new ArrayList<>();
+    	linhasArquivo.add("1111;1;222;222");
+
+    	try {
+			gerarArquivoTemp(linhasArquivo, nomeArquivoTemp);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			Assert.fail();
+		}
+
+    	ArquivoVO arquivoVO = new ArquivoVO();
+    	arquivoVO.setCaminho(nomeArquivoTemp);
+
+    	UploadDispositivoBusiness uploadDispositivoBusiness = new UploadDispositivoBusiness();
+
+    	try {
+    		uploadDispositivoBusiness.processarArquivo(arquivoVO);
+		} catch (Exception exception) {
+			List<String> criticas = uploadDispositivoBusiness.recuperarCriticas();
+			Assert.assertEquals("Linha: 1 - Erro: Valor da Data de Fabricação inválida", criticas.get(0));
+		}
+    }
+
+    /**
      * Gera um arquivo com as linhas solicitadas.
      * @param linhas
      * @throws IOException
@@ -82,5 +252,29 @@ public class UploadDispositivoTest {
 			throw e;
 		}
     }
+
+    private void apagarDispositivo(String id) {
+        DispositivoBusiness business = new DispositivoBusiness();
+        try {
+            business.apagarDispositivo(id);
+        } catch (BusinessException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private UsuarioVO getUsuario() {
+		UsuarioVO usuario = new UsuarioVO();
+
+        String admin = "admin";
+		usuario.setLogin(admin);
+        usuario.setSenha(admin);
+        usuario.setNomeUsuario(admin);
+
+        PerfilVO perfil = new PerfilVO();
+        perfil.setIdPerfil(1);
+        usuario.setPerfil(perfil);
+
+		return usuario;
+	}
 
 }
