d6d239d91d5b018fecf64b7159acd65d6cfbd4df
castilhodaniel <Daniel Castilho@192.168.1.102>
2012-09-01 16:40:11 -0300

    Correção do checkstyle.

--- fontes/GACCore/src/br/com/sw2/gac/modelo/Cliente.java
+++ fontes/GACCore/src/br/com/sw2/gac/modelo/Cliente.java
@@ -6,6 +6,7 @@ package br.com.sw2.gac.modelo;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -422,7 +423,11 @@ public class Cliente implements Serializable {
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (nmCPFCliente != null ? nmCPFCliente.hashCode() : 0);
+        if (nmCPFCliente != null) {
+            hash += nmCPFCliente.hashCode();
+        } else {
+            hash += 0;
+        }
         return hash;
     }
 
