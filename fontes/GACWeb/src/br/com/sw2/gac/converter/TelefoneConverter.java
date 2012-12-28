d41b1b122d7648c46870c337ff78f5d8e0d18463
roglusa <rogerio.santos@gmail.com>
2012-12-28 00:31:22 -0200

    Adequação do converter para o uso fora de páginas xhtml

--- fontes/GACWeb/src/br/com/sw2/gac/converter/TelefoneConverter.java
+++ fontes/GACWeb/src/br/com/sw2/gac/converter/TelefoneConverter.java
@@ -8,7 +8,7 @@ import javax.faces.convert.FacesConverter;
 import br.com.sw2.gac.util.StringUtil;
 
 /**
- * <b>Descrição: Converte um valor string em BiDecimal e um valor BigDecimal em String.</b> <br>
+ * <b>Descrição: Formata o telefone a ser exibido na tela.</b> <br>
  * .
  * @author: SW2
  * @version 1.0 Copyright 2012 SmartAngel.
@@ -35,18 +35,32 @@ public class TelefoneConverter implements Converter {
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
 
-        final int tamanhoCelular = 11;
-        final int tamanhoFixo = 10;
-        String maskCelular = "(##)#####-####";
-        String maskFixo = "(##)####-####";
+        return formatarTelefone(value.toString());
+    }
+
+    /**
+     * Nome: formatarTelefone
+     * Formatar telefone.
+     *
+     * @param value the value
+     * @return string
+     * @see
+     */
+    public String formatarTelefone(String value) {
         String retorno = null;
-        String numero = value.toString();
-        if (numero.length() == tamanhoCelular) {
-            retorno = StringUtil.formatString(numero, maskCelular);
-        } else if (numero.length() == tamanhoFixo) {
-            retorno = StringUtil.formatString(numero, maskFixo);
-        } else {
-            retorno = numero;
+        if (null != value) {
+            final int tamanhoCelular = 11;
+            final int tamanhoFixo = 10;
+            String maskCelular = "(##)#####-####";
+            String maskFixo = "(##)####-####";
+            String numero = value;
+            if (numero.length() == tamanhoCelular) {
+                retorno = StringUtil.formatString(numero, maskCelular);
+            } else if (numero.length() == tamanhoFixo) {
+                retorno = StringUtil.formatString(numero, maskFixo);
+            } else {
+                retorno = numero;
+            }
         }
         return retorno;
     }
