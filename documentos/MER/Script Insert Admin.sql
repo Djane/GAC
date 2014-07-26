eeb13f23e26700b475c8d34f97d58e23d1216964
Marcelo Santos <marcelo68ms@gmail.com>
2014-07-26 16:32:47 -0300

    Documentação do Intelix

---
+++ documentos/MER/Script Insert Admin.sql
@@ -0,0 +1,4 @@
+insert into tblusuario (login, nmusuario, senha, nmtelfixo, nmtelcelular, 
+nmfuncao, cdperfil, nmregistro, nmramal) values ('admin', 'Admin', 'YWRtaW4=', 
+NULL, NULL, NULL, '1', '1000', '10')
+
