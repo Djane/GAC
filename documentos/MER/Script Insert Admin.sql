831e539f610554a971cc6d384e70eb5089411c03
Marcelo Santos <marcelo68ms@gmail.com>
2014-07-26 16:54:09 -0300

    Exclusão da Constraint

--- documentos/MER/Script Insert Admin.sql
+++ documentos/MER/Script Insert Admin.sql
@@ -1,4 +1,6 @@
 insert into tblusuario (login, nmusuario, senha, nmtelfixo, nmtelcelular, 
 nmfuncao, cdperfil, nmregistro, nmramal) values ('admin', 'Admin', 'YWRtaW4=', 
-NULL, NULL, NULL, '1', '1000', '10')
+NULL, NULL, NULL, '1', '1000', '10');
+
+alter table tblcontrato drop foreign key reftblvendedor462;
 
